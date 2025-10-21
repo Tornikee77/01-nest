e6c78bb65f3da79eea6c302396e3f5f02fda3b95
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-10-21 20:53:07 +0400

    feature

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  // !Create the swagger configuration
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS blog API')
+    .setDescription('Use the base url: http://localhost:300')
+    .setTermsOfService('http://localhost:300/terms-of-service')
+    .setLicense('MIT licens', 'http://localhost:300/')
+    .addServer('http://localhost:300/')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,10 +2,33 @@ import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamsDto } from './dtos/get-users-params.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   @Get('{/:id}')
+  @ApiOperation({
+    summary: 'Fetch a list of registered users on the application',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: String,
+    description: 'The upper limit of page you want to pagination to returns ',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: String,
+    description: 'The upper limit of page you want to pagination to returns ',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: String,
+    description: 'The upper limit of page you want to pagination to returns ',
+    required: true,
+  })
   public getUsers(@Param() getUserParamDTo: GetUsersParamsDto) {
     console.log(getUserParamDTo);
     return `you send a get request to users endpoint`;
